5cbbf356887a7706d2a722160868d8613e575c9f
Joseph Cava <bink.lynch@gmail.com>
2025-02-18 21:55:41 -0800

    Add vulnerability data app

--- 00-Starter-Seed/server.js
+++ 00-Starter-Seed/server.js
@@ -4,6 +4,7 @@ const http = require('http');
 const logger = require('morgan');
 const path = require('path');
 const router = require('./routes/index');
+const { auth } = require('express-openid-connect');
 
 dotenv.load();
 
@@ -15,6 +16,26 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
+
+const config = {
+  authRequired: false,
+  auth0Logout: true
+};
+
+config.issuerBaseURL = process.env.ISSUER_BASE_URL;
+
+const port = process.env.PORT || 3000;
+if (!config.baseURL && !process.env.BASE_URL && process.env.PORT && process.env.NODE_ENV !== 'production') {
+  config.baseURL = `http://localhost:${port}`;
+}
+
+app.use(auth(config));
+
+// Middleware to make the `user` object available for all views
+app.use(function (req, res, next) {
+  res.locals.user = req.oidc.user;
+  next();
+});
 
 app.use('/', router);
 
@@ -34,9 +55,7 @@ app.use(function (err, req, res, next) {
   });
 });
 
-const port = process.env.PORT || '3000';
-
 http.createServer(app)
   .listen(port, () => {
-    console.log(`listening on http://localhost:${port}`);
+    console.log(`Listening on ${config.baseURL}`);
   });
